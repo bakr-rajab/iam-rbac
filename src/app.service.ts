7fae5794d627c23fb447f7df342e9d6ae79517f4
bakr-rajab <abobakr.r.bakry@gmail.com>
2023-07-29 22:17:11 +0300

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Wor55555ld!';
+    return 'Hello Wor55558885ld!';
   }
 }
